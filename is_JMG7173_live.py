f5bd9a569ad0934b3eef1f4fff143be27686685a
Minho Ryang <minhoryang@gmail.com>
2017-03-04 22:19:31 +0900

    Is @JMG7173 Live?

---
+++ is_JMG7173_live.py
@@ -0,0 +1,25 @@
+import requests
+
+
+class is_JMG7173_live_Exception(Exception):
+    pass
+
+
+jmg7173_channel_url = (
+    "https://www.youtube.com/channel/UCtpDukzC0DT31_eoM2YcxEQ"
+)
+
+
+def is_JMG7173_live() -> bool:
+    r = requests.get(jmg7173_channel_url)
+    if not r.ok:
+        raise is_JMG7173_live_Exception(
+            "Couldn't get JMG7173's Youtube Channel"
+        )
+    return 'yt-badge-live' in r.text
+
+
+if __name__ == "__main__":
+    print(
+        'Yes' if is_JMG7173_live() else 'No'
+    )
